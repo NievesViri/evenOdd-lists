9f728d8137019b24ad5c53225bd8990e90e77957
NievesViri <105406390+NievesViri@users.noreply.github.com>
2022-09-21 00:09:36 -0500

    fixed the reset

--- evenOdd.js
+++ evenOdd.js
@@ -6,19 +6,19 @@ let evenList = document.getElementById("even")
 function updateWord(element) {
   usersWord = element.value
 
-  console.log(usersWord)
+
 }
 function handleSubmit() {
     let newListItem = document.createElement('LI')
     newListItem.innerText = usersWord
 
     
-    if(usersWord.length %2 === 0) {
-        evenList.appendChild(newListItem)
+    if(usersWord.length % 2 === 0) {
+    evenList.appendChild(newListItem)
   } else {
     oddlist.appendChild(newListItem)
   }
 
   usersWord = ""
-  document.getElementById("even-odd-form").requestFullscreen()
+  document.getElementById("even-odd-form").reset()
 }
